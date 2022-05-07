d5f7c88a1d41ce17baed5438ed7beb90f0e9e9cf
sertac aslan <84998448+sertacaslan@users.noreply.github.com>
2022-05-07 23:50:06 +0300

    Add files via upload

---
+++ FakeData.cs
@@ -0,0 +1,23 @@
+﻿using ApiDene2.Models;
+using Bogus;
+
+namespace ApiDene2.Fake
+{
+    public static class FakeData
+    {
+        private static List<User> _users;
+        public static List<User> GetUsers(int number)
+        {
+            _users = new Faker<User>()
+                .RuleFor(p => p.Id, f => f.IndexFaker)
+                .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+                .RuleFor(p => p.LastName, f => f.Name.LastName())
+                .RuleFor(p => p.Address, f => f.Address.StreetAddress())
+                .Generate(number);
+            return _users;
+        }
+
+
+
+    }
+}
